de8e47feac67e9c0f7211b1205e6695b8b1d0cc8
Gordiig <gordiig@gmail.com>
2020-04-07 18:57:23 +0300

    Reduced some fields in RequestStats

--- RequestStats/tests.py
+++ RequestStats/tests.py
@@ -9,32 +9,22 @@ class RequestsStatsListTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
         self.path = self.url_prefix + 'requests/'
-        self.request_stat = RequestsStats.objects.create(method='GET', endpoint='ee', user_id=1, gateway_process_time=10,
-                                                         status_code=200, cb_end_state='CLOSED', cb_start_state='CLOSED',
-                                                         is_fully_processed=True, queue_length=True,
-                                                         request_dt='2020-03-12T14:15Z')
+        self.request_stat = RequestsStats.objects.create(method='GET', endpoint='ee', user_id=1, process_time=10,
+                                                         status_code=200, request_dt='2020-03-12T14:15Z')
         self.data_201_1 = {
             'method': 'GET',
             'user_id': 1,
             'endpoint': '/places/',
-            'gateway_process_time': 10,
             'status_code': 200,
-            'is_fully_processed': True,
-            'cb_start_state': 'CLOSED',
-            'cb_end_state': 'CLOSED',
-            'queue_length': 0,
+            'process_time': 100,
             'request_dt': '2020-03-12T14:15Z',
         }
         self.data_201_2 = {
             'method': 'GET',
-            'user_id': 1,
+            'user_id': None,
             'endpoint': '/places/',
-            'gateway_process_time': 10,
             'status_code': 200,
-            'is_fully_processed': True,
-            'cb_start_state': 'CLOSED',
-            'cb_end_state': 'CLOSED',
-            'queue_length': 0,
+            'process_time': 100,
             'request_dt': '2020-03-12T14:15Z',
         }
         self.data_400_1 = {
@@ -44,36 +34,16 @@ class RequestsStatsListTestCase(BaseTestCase):
             'method': 'WRONG',
             'user_id': 1,
             'endpoint': '/places/',
-            'gateway_process_time': 10,
             'status_code': 200,
-            'is_fully_processed': True,
-            'cb_start_state': 'CLOSED',
-            'cb_end_state': 'CLOSED',
-            'queue_length': 0,
+            'process_time': 100,
             'request_dt': '2020-03-12T14:15Z',
         }
         self.data_400_3 = {
             'method': 'GET',
             'user_id': 1,
             'endpoint': '/places/',
-            'gateway_process_time': 10,
             'status_code': 200,
-            'is_fully_processed': True,
-            'cb_start_state': 'WRONG',
-            'cb_end_state': 'WRONG',
-            'queue_length': 0,
-            'request_dt': '2020-03-12T14:15Z',
-        }
-        self.data_400_4 = {
-            'method': 'GET',
-            'user_id': 1,
-            'endpoint': '/places/',
-            'gateway_process_time': 10,
-            'status_code': 200,
-            'is_fully_processed': True,
-            'cb_start_state': 'WRONG',
-            'cb_end_state': 'WRONG',
-            'queue_length': 0,
+            'process_time': 100,
             'request_dt': '2020-03-12 14:15 ',
         }
 
@@ -93,7 +63,7 @@ class RequestsStatsListTestCase(BaseTestCase):
         _ = self.post_response_and_check_status(url=self.path, data=self.data_201_1)
 
     def testPost201_OKAnon(self):
-        _ = self.post_response_and_check_status(url=self.path, data=self.data_201_1)
+        _ = self.post_response_and_check_status(url=self.path, data=self.data_201_2)
 
     def testPost400_WrongJSON(self):
         _ = self.post_response_and_check_status(url=self.path, data=self.data_400_1, expected_status_code=400)
@@ -101,11 +71,8 @@ class RequestsStatsListTestCase(BaseTestCase):
     def testPost400_WrongMethod(self):
         _ = self.post_response_and_check_status(url=self.path, data=self.data_400_2, expected_status_code=400)
 
-    def testPost400_WrongCBState(self):
-        _ = self.post_response_and_check_status(url=self.path, data=self.data_400_3, expected_status_code=400)
-
     def testPost400_WrongDTFormat(self):
-        _ = self.post_response_and_check_status(url=self.path, data=self.data_400_4, expected_status_code=400)
+        _ = self.post_response_and_check_status(url=self.path, data=self.data_400_3, expected_status_code=400)
 
 
 class RequestsStatsTestCase(BaseTestCase):
@@ -114,10 +81,8 @@ class RequestsStatsTestCase(BaseTestCase):
     """
     def setUp(self):
         super().setUp()
-        self.request_stat = RequestsStats.objects.create(method='GET', endpoint='ee', user_id=1, gateway_process_time=10,
-                                                         status_code=200, cb_end_state='CLOSED', cb_start_state='CLOSED',
-                                                         is_fully_processed=True, queue_length=True,
-                                                         request_dt='2020-03-12T14:15Z')
+        self.request_stat = RequestsStats.objects.create(method='GET', endpoint='ee', user_id=1, process_time=10,
+                                                         status_code=200, request_dt='2020-03-12T14:15Z')
         self.path = self.url_prefix + f'requests/{self.request_stat.id}/'
         self.path_404 = self.url_prefix + f'requests/{self.request_stat.id+10000}/'
 
