7cae192f08b532c4a49337ed337515dc69998af2
Gordiig <gordiig@gmail.com>
2020-05-16 14:18:04 +0300

    Add heroku configs

--- StatsService/settings.py
+++ StatsService/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -151,6 +151,12 @@ except ImportError as e:
     raise e
 
 ALLOW_REQUESTS = True
+
+ON_HEROKU = not (os.getenv('ON_HEROKU', '0') == '0')
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals(), databases=ON_HEROKU, test_runner=False, secret_key=False)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
